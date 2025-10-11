cabf0ee9ac8c2ca1daf99637d8121c215aed9aca
sarah254-tech <sarahamadi97@gmail.com>
2025-10-11 09:41:17 +0000

    Final Edit log_cleaner.sh

--- log_cleaner.sh
+++ log_cleaner.sh
@@ -6,22 +6,36 @@ IFS=$'\n\t'
 
 LOG_DIR="/home/sara/sys_audit"
 mkdir -p "$LOG_DIR"
-LOG_FILE="$LOG_DIR/log_cleaner_$(date +%F).log"
+LOG_FILE="$LOG_DIR/$(basename "$0" .sh)_$(date +%F).log"
+HTML_FILE="$LOG_DIR/$(basename "$0" .sh)_$(date +%F).html"
 DAYS=7
 EMAIL="sarahamadi97@gmail.com"
-SUBJECT="Log CLeaner Report - $(date +%F_%H-%M)"
+SUBJECT="Log Cleaner Report - $(date +%F_%H-%M)"
 
 # Capture output
 exec > >(tee -a "$LOG_FILE") 2>&1
 
 find "$LOG_FILE" -type f -mtime +"$DAYS" -exec rm -f {} \;
-echo "$(date): Old logs cleaned successfully from $LOG_FILE" > "$LOG_DIR/log_cleaner_report.txt"
+echo "$(date): Old logs cleaned successfully from $LOG_FILE" >> "$LOG_FILE"
+
+
+# HTML  report
+{
+	echo "<html><body style='font-family:Arial;'>"
+    echo "<h2 style='color:#2E86C1;'>🧹 Log Cleaner Report</h2>"
+    echo "<p><b>Date:</b> $(date)</p><hr>"
+    echo "<pre style='background:#F8F9F9;padding:10px;border-radius:6px;'>"
+    cat "$LOG_FILE"
+    echo "</pre><hr>"
+    echo "<p style='color:#16A085;'>✅ Log cleaner completed successfully.</p>"
+    echo "</body></html>"
+} > "$HTML_FILE"
 
 # Send Email
-if [ -s "$LOG_FILE" ]; then
-	echo "sending log cleaner report:$LOG_FILE"
+if [ -s "$HTML_FILE" ]; then
+	echo "sending log cleaner report:$HTML_FILE"
 
-	mail -a "Content-Type: text/html" \ -s "$SUBJECT" \ "$EMAIL" < "$LOG_DIR/log_cleaner_report.log"
+	mutt -e "set content_type=text/html" -s "$SUBJECT" -- "$EMAIL" < "$HTML_FILE"
 	echo "Email sent to $EMAIL"
 
 else
