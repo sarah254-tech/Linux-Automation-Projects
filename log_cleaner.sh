dcf22eca6673a5127a8d3a6109924963aaa6c003
sarah254-tech <sarahamadi97@gmail.com>
2025-10-11 07:07:33 +0000

    Editted LOG_DIR to sys_audit

--- log_cleaner.sh
+++ log_cleaner.sh
@@ -2,19 +2,30 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Description: Scleans old Log files older than 7 days.
+# Description: Cleans old Log files older than 7 days.
 
 LOG_DIR="/home/sara/sys_audit"
+mkdir -p "$LOG_DIR"
+LOG_FILE="$LOG_DIR/log_cleaner_$(date +%F).log"
 DAYS=7
+EMAIL="sarahamadi97@gmail.com"
+SUBJECT="Log CLeaner Report - $(date +%F_%H-%M)"
 
-find "$LOG_DIR" -type f -mtime +"$DAYS" -exec rm -f {} \;
-echo "$(date): Old logs cleaned successfully from $LOG_DIR"
+# Capture output
+exec > >(tee -a "$LOG_FILE") 2>&1
+
+find "$LOG_FILE" -type f -mtime +"$DAYS" -exec rm -f {} \;
+echo "$(date): Old logs cleaned successfully from $LOG_FILE" > "$LOG_DIR/log_cleaner_report.txt"
 
 # Send Email
-if [ -f "$LOG_DIR" ]; then
-	echo "sending log cleaner report:
-$LOG_FILE"
-	mail -a "Content-Type: text" \ -s "Log cleaner text report" \ sarahamadi97@gmail.com < "$LOG_DIR"
+if [ -s "$LOG_FILE" ]; then
+	echo "sending log cleaner report:$LOG_FILE"
+
+	mail -a "Content-Type: text/html" \ -s "$SUBJECT" \ "$EMAIL" < "$LOG_DIR/log_cleaner_report.log"
+	echo "Email sent to $EMAIL"
+
 else
+
 echo "No report file found at $LOG_DIR"
+
 fi
